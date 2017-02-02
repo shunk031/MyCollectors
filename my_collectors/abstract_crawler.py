8662acca0a85c2f7d811f64d134351878fe00c9d
Shunsuke KITADA <septemtrio.ager@gmail.com>
2017-02-02 13:31:59 +0900

    add abstract class scripts

---
+++ my_collectors/abstract_crawler.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+
+import time
+import json
+
+from abc import ABCMeta, abstractmethod
+from base_collector import BaseCollector
+
+
+class AbstractCrawler(BaseCollector, metaclass=ABCMeta):
+
+    FINISH_CRAWL = "Finish crawl."
+
+    def __init__(self, scraper_class, target_url, save_dir="./data", page_count=1):
+
+        self.target_url = target_url
+        self.before_url = None
+        self.save_dir = save_dir
+        self.page_count = page_count
+
+        self.scraper = scraper_class(self.target_url, self.save_dir)
+
+    @abstractmethod
+    def get_next_page_link(self, url):
+        pass
+
+    def crawl(self):
+        try:
+            while True:
+                # start to measure the time
+                start = time.time()
+                print("[ PROCESS ] Now page {} PROCESSING".format(self.page_count))
+                self.scraper.scrap()  # scraping!
+                # get next page link url
+                self.target_url = self.get_next_page_link(self.target_url)
+
+                # if target_url is not found
+                if self.target_url is None:
+                    break
+
+                self.page_count += 1
+                time.sleep(2)
+                end = time.time()  # end to measure the time
+
+                # print processing time
+                self._print_processing_time(start, end)
+
+        except Exception as err:
+            print("[ EXCEPTION ] Exception occured: {}".format(err))
+
+            # save crawler status
+            self.save_crawler_status()
+
+        return self.FINISH_CRAWL
+
+    def _print_processing_time(self, start_time, end_time):
+
+        elapsed_sec = end_time - start_time
+        elapsed_min = elapsed_sec / 60
+
+        if elapsed_min < 1:
+            print("[ TIME ] Elapsed time: {:.2f} [sec]".format(elapsed_sec))
+        else:
+            print("[ TIME ] Elapsed time: {:.2f} [min]".format(elapsed_min))
+
+    def save_crawler_status(self):
+
+        status_dict = {}
+        status_dict["target_url"] = self.target_url
+        status_dict["page_count"] = self.page_count
+
+        with open("status.json", "w") as wf:
+            json.dump(status_dict, wf, indent=2)
+
+        print("[ SAVE ] Save status.json")
---
+++ my_collectors/abstract_scraper.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+
+import os
+import csv
+
+from abc import ABCMeta, abstractmethod
+from base_collector import BaseCollector
+
+
+class AbstractScraper(BaseCollector, metaclass=ABCMeta):
+
+    def __init__(self, target_url, save_dir):
+        self.target_url = target_url
+        self.save_dir = save_dir
+
+    def scrap(self):
+        # get list of detail article url
+        article_detail_url_list = self.get_article_detail_urls()
+
+        article_detail_info = []
+        for article_url in article_detail_url_list:
+            try:
+                article_dict = self.get_article_detail_info_dict(article_url)
+                article_detail_info.append(article_dict)
+            except Exception as e:
+                print(e)
+
+        self.save_article_detail_info_list(article_detail_info)
+
+    @abstractmethod
+    def get_article_detail_urls(self):
+        pass
+
+    @abstractmethod
+    def get_article_detail_info_dict(self, article_url):
+        pass
+
+    def create_article_dict(self, title, url, article):
+
+        article_dict = {}
+        article_dict["title"] = title
+        article_dict["url"] = url
+        article_dict["article"] = article
+
+        return article_dict
+
+    def save_article_detail_info_list(self, article_detail_info_list):
+
+        # if not exist save_dir, make the directory
+        if not os.path.isdir(self.save_dir):
+            os.makedirs(self.save_dir)
+
+        for article_detail_dict in article_detail_info_list:
+            article_title = article_detail_dict["title"]
+            csv_filename = self._convert_filename(article_title)
+            csv_filename = "{}.csv".format(csv_filename)
+
+            with open(os.path.join(self.save_dir, csv_filename), "w") as wf:
+                writer = csv.writer(wf)
+                writer.writerow([article_detail_dict["title"],
+                                 article_detail_dict["url"],
+                                 article_detail_dict["article"]])
+
+    def _convert_filename(self, article_title):
+
+        filename = article_title.replace(" ", "_")
+        filename = filename.replace("/", "")
+        filename = filename.replace("?", "")
+
+        if len(filename) > 250:
+            filename = filename[:250]
+        return filename
